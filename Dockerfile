294045b7492c36c0dd12258abb6905e9188bd4b2
Nikhil Verma <nikhilweee@gmail.com>
2024-05-30 02:22:03 +0000

    NPM Works

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,12 @@ FROM ubuntu:latest
 RUN apt update && \
     apt install --yes \
     apache2-utils \
-    nginx-extras \
-    python3-certbot \
-    python3-certbot-nginx
+    nginx-extras
 
-COPY nginx*.conf entrypoint.sh index.html /
+COPY nginx.conf entrypoint.sh /
 
 RUN chmod +x entrypoint.sh && \
+    mv nginx.conf /etc/nginx/nginx.conf && \
     ln -sf /dev/stdout /var/log/nginx/access.log && \
     ln -sf /dev/stderr /var/log/nginx/error.log
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-echo "Running Certbot"
+echo "Creating WebDAV Password"
 
-# Disable TLS on nginx conf
-mv nginx-http.conf /etc/nginx/nginx.conf
-mv index.html /var/www/html
-
-# Run certbot
-certbot certonly --nginx --non-interactive --agree-tos \
-  --domain $CERTBOT_DOMAIN --email $CERTBOT_EMAIL --cert-name latest
-
-# Re-enable TLS on nginx conf
-mv nginx-webdav.conf /etc/nginx/nginx.conf
-
-# Create webdav password
 htpasswd -b -c /etc/nginx/webdav.htpasswd \
   $WEBDAV_USER $WEBDAV_PASS > /dev/null 2>&1
 
